4242a26b21102723b0e681d8e2243c6051c40fe2
tjbouma <33105587+tjbouma@users.noreply.github.com>
2017-11-02 15:39:42 -0500

    Create Babysitter.java

---
+++ Babysitter.java
@@ -0,0 +1,56 @@
+/*
+ * 
+ * @TrevorBouma
+ */
+
+public class Babysitter {
+	private int startRate = 12;
+	private int bedtimeRate = 8;
+	private int midnightRate = 16;
+	private int bedtimePay;
+	private int midnightPay;
+	private int startPay;
+
+	public int calcStartPay(int start, int bedtime) throws Exception {
+		start = start + 12;
+		bedtime = bedtime + 12;
+		if (start < 17) {
+			throw new Exception("The babysitter can't start before 5.");
+		} else {
+
+			startPay = (bedtime - start) * startRate;
+		}
+
+		return startPay;
+	}
+
+	public int calcBedtimePay(int bedtime, int midnight) {
+		midnight = 24;
+		if (bedtime <= 12) {
+			bedtime = bedtime + 12;
+		}
+		bedtimePay = (midnight - bedtime) * bedtimeRate;
+
+		return bedtimePay;
+	}
+
+	public int calcMidnightPay(int midnight, int end) throws Exception {
+		midnight = 24;
+		end = end + 24;
+		if (end > 28) {
+			throw new Exception("The babysitter can't work after 4am.");
+		}
+		midnightPay = (end - 24) * midnightRate;
+		return midnightPay;
+	}
+	
+	public int calcTotalPay(int start, int bedtime, int end) throws Exception {
+		Babysitter babysitter = new Babysitter();
+		startPay = babysitter.calcStartPay(5, 9);
+		bedtimePay = babysitter.calcBedtimePay(9, 12);
+		midnightPay = babysitter.calcMidnightPay(12, 4);
+		int totalPay;
+		totalPay = (startPay + bedtimePay + midnightPay);
+		return totalPay;
+	}
+}
